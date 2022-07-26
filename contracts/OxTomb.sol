4d4d6c6c30e81811da224d5669dc452e27c6f469
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-07-26 18:42:51 +0800

    chore: 完成脚手架配置，实现编译

--- contracts/OxTomb.sol
+++ contracts/OxTomb.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,23 @@
-import { ethers } from "hardhat";
+import {ethers} from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    const [deployer] = await ethers.getSigners();
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    console.log("Deploying contracts with the account:", deployer.address);     // 部署人的地址
+    console.log("Account balance:", (await deployer.getBalance()).toString()); // 部署人的balance
 
-  await lock.deployed();
+    // 选择合约
+    const Token = await ethers.getContractFactory("oxTomb");
+    // 部署合约
+    const token = await Token.deploy("666w1", "666w2");
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+    console.log("Token address:", token.address);   // 部署后的合约地址
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
