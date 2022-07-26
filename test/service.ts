0a8c0db4f3a9292d371ec79d03fa0d8b420e4c54
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-07-26 17:44:13 +0800

    feat: 更新测试用例

--- test/service.ts
+++ test/service.ts
@@ -1,5 +1,4 @@
-import {time, loadFixture} from "@nomicfoundation/hardhat-network-helpers";
-import {anyValue} from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import {loadFixture} from "@nomicfoundation/hardhat-network-helpers";
 import {expect} from "chai";
 import {ethers} from "hardhat";
 
@@ -14,31 +13,31 @@ describe("oxTomb-service", function () {
         return {owner, otherAddress, oxTombInstance};
     }
 
-    const timeStamp = () => {
+    // 精确到秒
+    const unixTimeStamp = () => {
         let time = new Date().getTime() / 1000;
         return Math.floor(time);
     }
 
     describe("mint", async () => {
-        it("mint tokenID从1开始", async () => {
+        it("mint初始化,tokenID从1开始", async () => {
             const {owner, oxTombInstance} = await loadFixture(deployOxTombFixture);
             await oxTombInstance.mint(owner.address);
             const balance = await oxTombInstance.balanceOf(owner.address);
             const tokenOwner = await oxTombInstance.ownerOf(1);
-
             expect(balance).to.equal(1);
             expect(tokenOwner).to.equal(owner.address);
         })
-        it("mint transferTimes为1，user为当前用户地址", async () => {
+
+        it("mint初始化,user为当前用户地址,transferTimes为1,expires为 1 week", async () => {
             const {owner, oxTombInstance} = await loadFixture(deployOxTombFixture);
             await oxTombInstance.mint(owner.address);
+            const oneWeek = 60 * 60 * 24 * 7;
             const tokenInfo = await oxTombInstance.queryExpiresByTokenId(1);
-
+            const timeDifference = Math.abs(tokenInfo[1].toNumber() - unixTimeStamp() - oneWeek)
             expect(tokenInfo[0]).to.equal(owner.address);
+            expect(timeDifference < 60).to.equal(true); // 误差 < 1 minute
             expect(tokenInfo[2].toNumber()).to.equal(1);
         })
-        it("mint 时过期时间默认为+7天(hardhatBug)", async () => {
-        })
     })
-
 })
